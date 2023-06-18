32322507d2db6bda2a5a4e203c90eddb31801983
Vitaliy <bezuglivita123@gmail.com>
2023-06-18 21:43:46 +0300

    commands

--- src/Cryptocurrencies.DesktopUi/ViewModels/MainWindowViewModel.cs
+++ src/Cryptocurrencies.DesktopUi/ViewModels/MainWindowViewModel.cs
@@ -8,15 +8,29 @@ public class MainWindowViewModel : ViewModelBase
 {
     private readonly FrameContainer _frameContainer;
     private readonly CurrencyPage _currencyPage;
+    private readonly CoinsPage _coinsPage;
+    private readonly ConvertPage _convertPage;
 
-    public MainWindowViewModel(FrameContainer frameContainerContainer, CurrencyPage currencyPage)
+    public MainWindowViewModel(FrameContainer frameContainerContainer, CurrencyPage currencyPage, CoinsPage coinsPage, ConvertPage convertPage)
     {
         _frameContainer = frameContainerContainer;
         _currencyPage = currencyPage;
+        _coinsPage = coinsPage;
+        _convertPage = convertPage;
     }
 
     public ICommand GoToCurrencyCommand => new CommandHandler(() =>
     {
         _frameContainer.NavigationFrame.NavigationService.Navigate(_currencyPage);
+    }, () => true);
+    
+    public ICommand GoToCoinsCommand => new CommandHandler(() =>
+    {
+        _frameContainer.NavigationFrame.NavigationService.Navigate(_coinsPage);
+    }, () => true);
+    
+    public ICommand GoToConvertCommand => new CommandHandler(() =>
+    {
+        _frameContainer.NavigationFrame.NavigationService.Navigate(_convertPage);
     }, () => true);
 }
