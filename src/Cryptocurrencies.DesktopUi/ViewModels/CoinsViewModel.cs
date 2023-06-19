170ea1dfe03450a4979636b7aeecb0378406fc03
Vitaliy <bezuglivita123@gmail.com>
2023-06-19 13:14:55 +0300

    ordering

--- src/Cryptocurrencies.DesktopUi/ViewModels/CoinsViewModel.cs
+++ src/Cryptocurrencies.DesktopUi/ViewModels/CoinsViewModel.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Threading.Tasks;
 using Cryptocurrencies.Application.Coins.GetAllCoinsQuery;
 using Cryptocurrencies.Application.Common.Models;
@@ -9,12 +11,16 @@ namespace Cryptocurrencies.DesktopUi.ViewModels;
 public class CoinsViewModel : ViewModelBase
 {
     private readonly IMediator _mediator;
+    private int _limit;
+    private string _nameToFind;
     private ObservableCollection<CoinModel> _coins;
-    
+
     public CoinsViewModel(IMediator mediator)
     {
         _mediator = mediator;
-
+        
+        _limit = 10;
+        _nameToFind = string.Empty;
         _coins = GetInitialCoins();
     }
 
@@ -24,9 +30,25 @@ public class CoinsViewModel : ViewModelBase
         set => SetField(ref _coins, value);
     }
     
+    public int Limit
+    {
+        get => _limit;
+        set => SetField(ref _limit, value);
+    }
+    
+    public string NameToFind
+    {
+        get => _nameToFind;
+        set
+        {
+            SetField(ref _nameToFind, value);
+            OrderCoins(_nameToFind);
+        }
+    }
+    
     public async Task LoadCoinsAsync()
     {
-        var coins = await _mediator.Send(new GetAllCoinsQuery(10));
+        var coins = await _mediator.Send(new GetAllCoinsQuery(Limit));
         
         Coins.Clear();
         foreach (var coin in coins)
@@ -34,7 +56,7 @@ public class CoinsViewModel : ViewModelBase
             Coins.Add(coin);
         }
     }
-    
+
     private ObservableCollection<CoinModel> GetInitialCoins()
     {
         return new ObservableCollection<CoinModel>
@@ -46,5 +68,18 @@ public class CoinsViewModel : ViewModelBase
                 MaxSupply = 21000000d, VolumeUsd24Hr = 1000000000m, Vwap24Hr = 10000m
             }
         };
+    }
+    
+    private void OrderCoins(string filter)
+    {
+        var filteredCoins = Coins
+            .OrderByDescending(c => c.Name.Contains(filter, StringComparison.OrdinalIgnoreCase))
+            .ToList();
+        
+        Coins.Clear();
+        foreach (var coin in filteredCoins)
+        {
+            Coins.Add(coin);
+        }
     }
 }
